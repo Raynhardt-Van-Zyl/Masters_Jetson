e45674c1979a8145fde6832ce5e57fbd0b7fd51d
Raynhardt <raynhardt.vanzyl@gmail.com>
2021-02-22 13:37:27 +0200

    Removed stuff

---
+++ scripts/camera_test.py
@@ -0,0 +1,103 @@
+# --------------------------------------------------------
+# Camera sample code for Tegra X2/X1
+#
+# This program could capture and display video from
+# IP CAM, USB webcam, or the Tegra onboard camera.
+# Refer to the following blog post for how to set up
+# and run the code:
+#   https://jkjung-avt.github.io/tx2-camera-with-python/
+#
+# Written by JK Jung <jkjung13@gmail.com>
+# --------------------------------------------------------
+
+
+import sys
+import argparse
+import subprocess
+
+import cv2
+
+WINDOW_NAME = 'CameraDemo'
+
+def open_cam_onboard(width, height):
+    gst_elements = str(subprocess.check_output('gst-inspect-1.0'))
+    if 'nvcamerasrc' in gst_elements:
+        # On versions of L4T prior to 28.1, add 'flip-method=2' into gst_str
+        gst_str = ('nvcamerasrc ! '
+                   'video/x-raw(memory:NVMM), '
+                   'width=(int)2592, height=(int)1458, '
+                   'format=(string)I420, framerate=(fraction)30/1 ! '
+                   'nvvidconv ! '
+                   'video/x-raw, width=(int){}, height=(int){}, '
+                   'format=(string)BGRx ! '
+                   'videoconvert ! appsink').format(width, height)
+    elif 'nvarguscamerasrc' in gst_elements:
+        gst_str = ('nvarguscamerasrc ! '
+                   'video/x-raw(memory:NVMM), '
+                   'width=(int)1920, height=(int)1080, '
+                   'format=(string)NV12, framerate=(fraction)30/1 ! '
+                   'nvvidconv flip-method=2 ! '
+                   'video/x-raw, width=(int){}, height=(int){}, '
+                   'format=(string)BGRx ! '
+                   'videoconvert ! appsink').format(width, height)
+    else:
+        raise RuntimeError('onboard camera source not found!')
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+
+def open_window(width, height):
+    cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(WINDOW_NAME, width, height)
+    cv2.moveWindow(WINDOW_NAME, 0, 0)
+    cv2.setWindowTitle(WINDOW_NAME, 'Camera Demo for Jetson TX2/TX1')
+
+
+def read_cam(cap):
+    show_help = True
+    full_scrn = False
+    help_text = '"Esc" to Quit, "H" for Help, "F" to Toggle Fullscreen'
+    font = cv2.FONT_HERSHEY_PLAIN
+    while True:
+        if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
+            # Check to see if the user has closed the window
+            # If yes, terminate the program
+            break
+        _, img = cap.read() # grab the next image frame from camera
+        if show_help:
+            cv2.putText(img, help_text, (11, 20), font,
+                        1.0, (32, 32, 32), 4, cv2.LINE_AA)
+            cv2.putText(img, help_text, (10, 20), font,
+                        1.0, (240, 240, 240), 1, cv2.LINE_AA)
+        cv2.imshow(WINDOW_NAME, img)
+        key = cv2.waitKey(10)
+        if key == 27: # ESC key: quit program
+            break
+        elif key == ord('H') or key == ord('h'): # toggle help message
+            show_help = not show_help
+        elif key == ord('F') or key == ord('f'): # toggle fullscreen
+            full_scrn = not full_scrn
+            if full_scrn:
+                cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN,
+                                      cv2.WINDOW_FULLSCREEN)
+            else:
+                cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN,
+                                      cv2.WINDOW_NORMAL)
+
+
+def main():
+    print('OpenCV version: {}'.format(cv2.__version__))
+
+    cap = open_cam_onboard(1920, 1080)
+
+    if not cap.isOpened():
+        sys.exit('Failed to open camera!')
+
+    open_window(1920, 1080)
+    read_cam(cap)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
