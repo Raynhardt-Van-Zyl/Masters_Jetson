7a125904bba6118b17df0019e6aa8532b46841c1
Raynhardt <raynhardt.vanzyl@gmail.com>
2021-02-23 11:19:28 +0200

    1

--- Camera_Cpp/gstreamer_view.cpp
+++ Camera_Cpp/gstreamer_view.cpp
@@ -15,7 +15,7 @@
 
 std::string get_tegra_pipeline(int width, int height, int fps) {
     return "nvcamerasrc ! video/x-raw(memory:NVMM), width=" + std::to_string(width) + ", height=" +
-           std::to_string(height) + ", format=(string)I420, framerate=" + std::to_string(fps) +
+           std::to_string(height) + ", format=(string)I420, framerate=(fraction)" + std::to_string(fps) +
            "/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
 }
 
